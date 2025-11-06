b215c10572f4c84019c36e87219a7018ab3ef7d0
rafid4507 <rafid.4507@gmail.com>
2025-11-06 01:22:54 +0100

    requirtements set up

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='daktari_sheba_ai',
-    version='0.0.0',
+    version='0.1',
     packages=find_packages(),
+    author='Rafid',
     author_email='rafid.4507@gmail.com',
     install_requires=[],
 )
